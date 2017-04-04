6decc41872cce14efa90f5b26f376373ae74b47c
went.out <went.out@gmail.com>
2017-04-04 15:26:59 +0300

    Additions 4 first initiation of command when browser just started and tab inactive. Therefore at first call tab will be focused, and only then script will run.

--- background.js
+++ background.js
@@ -14,16 +14,57 @@ const getTabsList = (cb) => {
 	});
 }
 
+
+const activateTab = (tabId, cb) => {
+	chrome.tabs.update(tabId, {
+		'active' : true
+	}, cb);
+};
+const makeAction = (tab, cb) => {
+	chrome.tabs.executeScript(tab.id, {
+		file : `page${tab.audible}.js`
+	}, cb);
+};
+
+let firstRound = true;
+const finAction = (YMTab, activeTabId) => {
+	setTimeout(() => {
+		makeAction(YMTab, () => {
+			setTimeout(() => {
+				activateTab(activeTabId, () => {
+					firstRound = false;
+				});
+			}, 2000);
+		});
+	}, 1000);
+};
+
+
 chrome.commands.onCommand.addListener((command) => {
 	getTabsList((err, list) => {
+		let YMTab = null;
+		let activeTabId = null;
 		list.forEach((tab) => {
 			if (tab.pinned && tab.url.indexOf('music.yandex.ru') > 0) {
-				chrome.tabs.executeScript(tab.id, {
-					file : `page${tab.audible}.js`
-				}, () => {
-					// toggle = !toggle;
-				});
+				YMTab = tab;
+			}
+			if (tab.active) {
+				activeTabId = tab.id;
 			}
 		});
+		if (YMTab) {
+			if (firstRound) {
+				const checkAndFin = (tab) => {
+					if (tab.status == 'complete' && tab.selected) {
+						finAction(YMTab, activeTabId);
+					} else {
+						checkAndFin(tab);
+					}
+				};
+				activateTab(YMTab.id, checkAndFin);
+			} else {
+				makeAction(YMTab);
+			}
+		}
 	});
 });
